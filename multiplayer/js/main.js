a3d08813f9913c63e9a6311ec55a07662ad98bd9
DausterBarbosa <dausterbarbosa12@gmail.com>
2021-05-28 21:11:06 -0300

    timer adicionado

--- multiplayer/js/main.js
+++ multiplayer/js/main.js
@@ -6,10 +6,18 @@ let newGame = new Game({
     socket,
 });
 
-let element = document.getElementById("play");
 let modal = document.getElementById("content");
+let count = document.getElementById("counter");
 
-element.addEventListener("click", () => {
-    modal.style.display = "none";
-    newGame.iniciarJogo();
-});
+let counter = 5;
+let timer = setInterval(() => {
+    if( counter == 0 ) {
+        clearInterval(timer);
+        modal.style.display = "none";
+        newGame.iniciarJogo();
+    }
+    else{
+        count.innerHTML = `${counter}`;
+        counter--;
+    }
+}, 1000);
--- singleplayer/js/script.js
+++ singleplayer/js/script.js
@@ -65,7 +65,7 @@ function verificaColisao(){
     for(let i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert("asdfasdf");
+            alert("Você perdeu o jogo!!!");
         }
     }
 }
