5a92358fc5ff46a92658025746ed7139011c56b1
DausterBarbosa <dausterbarbosa12@gmail.com>
2021-05-29 09:30:15 -0300

    multiplayer configurado

--- multiplayer/js/main.js
+++ multiplayer/js/main.js
@@ -9,15 +9,22 @@ let newGame = new Game({
 let modal = document.getElementById("content");
 let count = document.getElementById("counter");
 
-let counter = 5;
-let timer = setInterval(() => {
-    if( counter == 0 ) {
-        clearInterval(timer);
-        modal.style.display = "none";
-        newGame.iniciarJogo();
-    }
-    else{
-        count.innerHTML = `${counter}`;
-        counter--;
-    }
-}, 1000);
+socket.on("startgame", () => {
+    let counter = 5;
+
+    let timer = setInterval(() => {
+        if( counter == 0 ) {
+            clearInterval(timer);
+            modal.style.display = "none";
+            newGame.iniciarJogo();
+        }
+        else{
+            count.innerHTML = `${counter}`;
+            counter--;
+        }
+    }, 1000);
+});
+
+socket.on("restartgame", () => {
+    window.location.reload();
+});
